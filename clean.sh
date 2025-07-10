7d77d0ba28b3bb13ab8d481ca69b31ddc6e640a7
prslc <prslc113@gmail.com>
2025-07-10 19:32:45 +0800

    change: use magiskboot provided cleanup function
    Signed-off-by: prslc <prslc113@gmail.com>

--- clean.sh
+++ clean.sh
@@ -1,11 +1,10 @@
 MODDIR="${0%/*}"
 
 # 判断并删除当前目录下的文件
-[ -e "kernel" ] && rm "kernel"
+[ -e "kernel" ] && ${MODDIR}/libmagiskboot.so --cleanup 2>/dev/null
 [ -e "new-boot.img" ] && rm "new-boot.img"
-[ -e "ramdisk.cpio" ] && rm "ramdisk.cpio"
 
-# 判断并删除 MODDIR/kpm 目录下的文件
+# 判断并删除 kpm 目录下的文件
 [ -e "$MODDIR/kpm/patched_kernel" ] && rm "$MODDIR/kpm/patched_kernel"
 [ -e "$MODDIR/kpm/kernel" ] && rm "$MODDIR/kpm/kernel"
 
